6ff72f0d101c7d1f7b3cb0da3f5ef6212da2d293
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-28 21:02:06 +0530

    added association with  and  models inside  function

--- node-sequelize/models/students.js
+++ node-sequelize/models/students.js
@@ -10,7 +10,16 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      // Association here
+      Students.belongsTo(models.Classroom, {
+        foreignKey: 'classroom_id',
+        as: 'classroom'
+      });
+      Students.belongsToMany(models.Course, {
+        through: 'StudentCourse',
+        as: 'courses',
+        foreignKey: 'student_id'
+      });
     }
   };
   Students.init({
