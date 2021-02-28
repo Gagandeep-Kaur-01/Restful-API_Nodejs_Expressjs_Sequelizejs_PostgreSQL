93b91f839444e8d8558eef2f52e27b1ab3edc62b
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-28 20:15:15 +0530

    created model for 'Course'

---
+++ node-sequelize/migrations/20210228143853-create-course.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Courses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      Lecturer_id: {
+        type: Sequelize.INTEGER
+      },
+      Course_name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Courses');
+  }
+};
---
+++ node-sequelize/models/course.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Course extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Course.init({
+    Lecturer_id: DataTypes.INTEGER,
+    Course_name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Course',
+  });
+  return Course;
+};
