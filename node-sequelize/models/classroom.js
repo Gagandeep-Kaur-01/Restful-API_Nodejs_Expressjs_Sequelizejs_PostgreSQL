e1d71c3391560fe984d6f481c7f53de831655e08
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-28 13:01:54 +0530

    created model for 'Classroom'

---
+++ node-sequelize/models/classroom.js
@@ -0,0 +1,23 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Classroom extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Classroom.init({
+    class_name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Classroom',
+  });
+  return Classroom;
+};
