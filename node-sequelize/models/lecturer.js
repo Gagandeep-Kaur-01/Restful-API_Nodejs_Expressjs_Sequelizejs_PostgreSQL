f89fbf8c7cff54cc2aaa9b536cd58e1435ae939f
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-28 22:00:14 +0530

    added association with 'Course' model inside 'associate' function

--- node-sequelize/models/lecturer.js
+++ node-sequelize/models/lecturer.js
@@ -10,7 +10,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      // Association here
+      Lecturer.hasOne(models.Course, {
+        foreignKey: 'lecturer_id',
+        as: 'course',
+      });
     }
   };
   Lecturer.init({
