b26b178eb66a5860bc332a936c0c96329f02f6ce
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-03-02 09:28:03 +0530

    declared classroom controller

--- node-sequelize/routes/index.js
+++ node-sequelize/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const classroomController = require('../controllers').classroom;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
