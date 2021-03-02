5cadd8c0b1115c2fce6deab3f5154fbd77f7d3a0
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-03-02 09:31:29 +0530

    added routes for classroom controller

--- node-sequelize/routes/index.js
+++ node-sequelize/routes/index.js
@@ -8,4 +8,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Classroom Router */
+router.get('/api/classroom', classroomController.list);
+router.get('/api/classroom/:id', classroomController.getById);
+router.post('/api/classroom', classroomController.add);
+router.put('/api/classroom/:id', classroomController.update);
+router.delete('/api/classroom/:id', classroomController.delete);
+
 module.exports = router;
