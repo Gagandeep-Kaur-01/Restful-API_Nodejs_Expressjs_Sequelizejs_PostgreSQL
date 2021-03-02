8fd1f7aa8aed8b6cef80cd0759a7eb5455cb3fa2
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-03-02 19:53:20 +0530

    added association to make a lecturer include a course

--- node-sequelize/controllers/lecturer.js
+++ node-sequelize/controllers/lecturer.js
@@ -46,6 +46,21 @@ module.exports = {
       .catch((error) => res.status(400).send(error));
   },
 
+  addWithCourse(req, res) {
+    return Lecturer
+      .create({
+        lecturer_name: req.body.lecturer_name,
+        course: req.body.course
+      }, {
+        include: [{
+          model: Course,
+          as: 'course'
+        }]
+      })
+      .then((lecturer) => res.status(201).send(lecturer))
+      .catch((error) => res.status(400).send(error));
+  },
+
   update(req, res) {
     return Lecturer
       .findByPk(req.params.id, {
