8ea86a627a31854f1111128acd815a551af7888e
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-03-02 19:31:44 +0530

    registered the Course controller

--- node-sequelize/controllers/index.js
+++ node-sequelize/controllers/index.js
@@ -1,9 +1,12 @@
 const classroom = require('./classroom');
 const student = require('./student');
 const lecturer = require('./lecturer');
+const course = require('./course');
+
 
 module.exports = {
   classroom,
   student,
   lecturer,
+  course,
 };
